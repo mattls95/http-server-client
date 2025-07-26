a6f64a809651bd82a37086442f39476ddcfc36b5
mattls <mattias.swenne@gmail.com>
2025-07-26 13:17:21 +0200

    add receive_new_clients

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 1024  # Port to listen on (non-privileged ports are > 1023)
@@ -30,6 +31,23 @@ def remove_client(client):
     print(f"{username} disconnected")
     send_message(f"{username} has left the chat")
     usernames.remove(username)
+
+def receive_new_clients(server):
+    server.listen()
+    print(f"Server running on{HOST}:{PORT}")
+
+    while True:
+        client,address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send("User")
+        username = client.recv(1024)
+        usernames.append(username)
+        clients.append(client)
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
