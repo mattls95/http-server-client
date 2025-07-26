7045920478edb60d6fd7a8d6a2d43e7fad323dab
mattls <mattias.swenne@gmail.com>
2025-07-26 13:29:34 +0200

    add server_init

---
+++ src/server_init.py
@@ -0,0 +1,10 @@
+import socket
+import os
+from server import receive_new_clients
+
+def init():
+    HOST = os.environ.get("HOST")
+    PORT = os.environ.get("PORT")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    receive_new_clients(server)
